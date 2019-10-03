e30f7caeff577b66c3c6d4cc38625584057afa37
kash01 <r.kashifshahzad@outlook.com>
2019-10-03 12:12:51 +0500

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./App.css";
 import Navigate from "./components/NavBar"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Car from "./components/Carousel";
+import Foot from "./components/Footer"
+import DetailPan from "./components/Detail";
+import Services from "./components/Service";
+import SMedia from "./components/SM";
 
 const App: React.FC = () => {
   return <div className="App">
     <Navigate />
+    <Car />
+    <Services />
+    <SMedia />
+    <DetailPan />
+    <Foot />
   </div>;
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 
 export interface NavigateProps {
 
@@ -13,14 +17,23 @@ class Navigate extends React.Component<NavigateProps, NavigateState> {
     state = {}
     render() {
         return (<div>
-            <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">
-                    Navbar{" "}
-                    <span className="badge badge-secondary badge-pill">
-                        Nav
-                     </span>
-                </a>
-            </nav>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">DowntownBliss</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">About Us</Nav.Link>
+                    <Nav.Link href="#features">Our Menu</Nav.Link>
+                    <Nav.Link href="#pricing">Catering</Nav.Link>
+                    <Nav.Link href="#pricing">Deals & Rewards</Nav.Link>
+                    <Nav.Link href="#pricing">Locate Us</Nav.Link>
+                </Nav>
+                <Nav.Link inline href="#pricing">Login</Nav.Link>
+                <Button variant="outline-info">Search</Button>
+                {/* <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form> */}
+            </Navbar>
+            <br />
         </div>);
     }
 }
